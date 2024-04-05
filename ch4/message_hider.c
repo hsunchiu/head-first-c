7d8f9a64d7366cbd4352449739171ae653a40515
hsun <hsun@hsuns-Air.lan>
2024-04-06 07:43:30 +0800

    finish ch4 encrypt part

---
+++ ch4/message_hider.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "encrypt.h"
+
+int main(){
+	char msg[80];
+	while(fgets(msg,sizeof(msg),stdin)){
+		encrypt(msg);
+		printf("%s",msg);
+	}
+}
---
+++ ch4/totaller.h
@@ -0,0 +1,2 @@
+#include <stdio.h>
+float add_with_tax(float f);
