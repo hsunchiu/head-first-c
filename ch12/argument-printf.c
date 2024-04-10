eede47bc22d4384250ce17c973d4409645c4d7ad
hsunchiu <qiuxunxun@yahoo.com>
2024-04-10 23:57:04 +0800

    add ch12

---
+++ ch12/argument-printf.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <pthread.h>
+
+void error(char *msg){
+	fprintf(stderr, "%s: %s\n", msg, strerror(errno));
+	exit(1);
+}
+
+void * does_not(void *a){
+	(void)a;
+	int i  = 0;
+	for (i = 0; i < 5; i++){
+		sleep(1);
+		printf("Does not!\n");
+	}
+
+	return NULL;
+}
+
+void * does_too(void *a){
+	(void)a;
+	int i  = 0;
+	for (i = 0; i < 5; i++){
+		sleep(1);
+		printf("Does two!\n");
+	}
+	return NULL;
+}
+
+int main(){
+	pthread_t t0; // record all the information about the thread
+	pthread_t t1;
+
+	if(pthread_create(&t0, NULL, does_not, NULL) == -1)
+		error("Can't create thread t0");
+	if(pthread_create(&t1, NULL, does_too, NULL) == -1)
+		error("Can't create thread t0");
+
+	/* wait for the thread to finish */
+	void *result;
+	if(pthread_join(t0, &result) == -1)
+		error("Can't join thread t0");
+	if(pthread_join(t1, &result) == -1)
+		error("Can't join thread t1");
+
+	return 0;
+}
---
+++ ch12/beer_fixed_strategy_2.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <pthread.h>
+
+int beers = 2000000;
+
+void error(char *msg)
+{
+    fprintf(stderr, "%s: %s\n", msg, strerror(errno));
+    exit(1);
+}
+
+pthread_mutex_t beers_lock = PTHREAD_MUTEX_INITIALIZER;
+void* drink_lots()
+{
+  int i;
+  for (i = 0; i < 100000; i++) {
+    pthread_mutex_lock(&beers_lock);
+    beers = beers - 1;
+    pthread_mutex_unlock(&beers_lock);
+  }
+  printf("beers = %i\n", beers);
+  return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+    pthread_t threads[20];
+    int t;
+    printf("%i bottles of beers on the wall\n%i bottles of beer\n", beers,beers);
+    for (t = 0; t < 20; t++)
+    {
+        if (pthread_create(&threads[t], NULL, drink_lots, NULL) == -1)
+            error("Unable to start thread");
+    }
+    void *result;
+    for (t = 0; t < 20; t++)
+    {
+        if (pthread_join(threads[t], &result) == -1)
+            error("Unable to join thread");
+    }
+    printf("There are now %i bottles of beers on the wall\n", beers);
+    return 0;
+}
